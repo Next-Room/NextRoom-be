ae44152add17a1dd6f8ebf66aca19681ecdfb962
eunsol-an <dev.eunsol@gmail.com>
2023-07-11 23:25:15 +0900

    initial commit

---
+++ src/main/java/com/todayescape/TodayEscapeApplication.java
@@ -0,0 +1,13 @@
+package com.todayescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayEscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayEscapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todayescape/TodayEscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayEscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
