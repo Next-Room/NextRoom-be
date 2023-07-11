a5467508f961f708e2a5b1e2e6ccf2cf6e66a1ca
Gibeom Lim <delphox60@gmail.com>
2023-07-12 00:17:29 +0900

    feat: define theme entity

--- src/main/java/com/todayescape/domain/Theme.java
+++ src/main/java/com/todayescape/domain/Theme.java
@@ -1,4 +1,26 @@
 package com.todayescape.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Theme {
+    @Id
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+    private String title;
+    private Long timeLimit;
+    private Long hintCount;
+    @OneToMany(mappedBy = "theme", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Hint> hints = new ArrayList<>();
 }
