afa85d14d571917f930a1740e6af321cc263e986
Gibeom Lim <delphox60@gmail.com>
2023-07-12 00:17:23 +0900

    feat: define shop entity

--- src/main/java/com/todayescape/domain/Shop.java
+++ src/main/java/com/todayescape/domain/Shop.java
@@ -1,4 +1,19 @@
 package com.todayescape.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Shop {
+    @Id
+    private Long id;
+    private String code;
+    private String name;
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Theme> themes = new ArrayList<>();
 }
