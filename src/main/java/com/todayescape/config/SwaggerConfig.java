7410a93308b8f97edf2ab720e9109c007c7745b0
Eunsol An <esan@anipen.com>
2023-07-13 18:17:09 +0900

    [FEAT] Springdoc Swagger 2.0.2 적용
    - http://localhost:8080/swagger-ui/index.html 로 접속
    - Swagger 테스트를 위해 TestController 추가, 추후 삭제 예정

---
+++ src/main/java/com/todayescape/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.todayescape.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(this.apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("O Escape Api Server")
+                .description("Nexters 23th 오늘의 방탈출 -NextRoom")
+                .version("1.0")
+                .contact(new Contact()
+                        .name("O Escape Server Github Repository")
+                        .url("https://github.com/Nexters/today-escape-server"));
+    }
+}
