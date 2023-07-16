118c24ce1f7e388d7dcad944760daf67083e32df
Gibeom Lim <delphox60@gmail.com>
2023-07-16 12:24:07 +0900

    [FIX] h2 콘솔 엑세스를 위한 필터체인 구현

--- src/main/java/com/todayescape/config/SecurityConfig.java
+++ src/main/java/com/todayescape/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.todayescape.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -21,5 +22,25 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
             .requestMatchers("/h2-console/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                    authorizationManagerRequestMatcherRegistry
+                        .requestMatchers("/h2-console/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated();
+
+                }
+            )
+            .httpBasic(Customizer.withDefaults())
+            .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.ignoringRequestMatchers(
+                new AntPathRequestMatcher("/h2-console/**")))
+            .headers(httpSecurityHeadersConfigurer -> {
+                httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin);
+            });
+        return http.build();
     }
 }
