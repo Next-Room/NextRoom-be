3b57befcd3c7d016389e62b36e30e9fee6757677
Gibeom Lim <delphox60@gmail.com>
2023-07-19 22:17:51 +0900

    [FEAT] 인증 정보로부터 shop 객체 가져오기 구현

--- src/main/java/com/nextroom/oescape/controller/ThemeController.java
+++ src/main/java/com/nextroom/oescape/controller/ThemeController.java
@@ -3,6 +3,7 @@ package com.nextroom.oescape.controller;
 import static com.nextroom.oescape.exceptions.StatusCode.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,8 @@ import com.nextroom.oescape.domain.Shop;
 import com.nextroom.oescape.dto.BaseResponse;
 import com.nextroom.oescape.dto.DataResponse;
 import com.nextroom.oescape.dto.ThemeDto;
+import com.nextroom.oescape.repository.ShopRepository;
+import com.nextroom.oescape.service.ShopService;
 import com.nextroom.oescape.service.ThemeService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,30 +27,37 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ThemeController {
     private final ThemeService themeService;
+    private final ShopService shopService;
 
     @PostMapping
     public ResponseEntity<BaseResponse> addTheme(
+        Authentication authentication,
         @RequestBody ThemeDto.AddThemeRequest request) {
-        themeService.addTheme(new Shop(), request);
+        Shop shop = shopService.extractShopFromAuthentication(authentication);
+        themeService.addTheme(shop, request);
         return ResponseEntity.ok(new BaseResponse(OK));
     }
 
     @GetMapping
-    public ResponseEntity<BaseResponse> getThemeList() {
-        return ResponseEntity.ok(new DataResponse<>(OK, themeService.getThemeList(new Shop())));
+    public ResponseEntity<BaseResponse> getThemeList(Authentication authentication) {
+        Shop shop = shopService.extractShopFromAuthentication(authentication);
+        return ResponseEntity.ok(new DataResponse<>(OK, themeService.getThemeList(shop)));
     }
 
     @PutMapping
-    public ResponseEntity<BaseResponse> editTheme(
+    public ResponseEntity<BaseResponse> editTheme(Authentication authentication,
         @RequestBody ThemeDto.EditThemeRequest request) {
-        themeService.editTheme(new Shop(), request);
+        Shop shop = shopService.extractShopFromAuthentication(authentication);
+        themeService.editTheme(shop, request);
         return ResponseEntity.ok(new BaseResponse(OK));
     }
 
     @DeleteMapping
     public ResponseEntity<BaseResponse> removeTheme(
+        Authentication authentication,
         @RequestBody ThemeDto.RemoveRequest request) {
-        themeService.removeTheme(new Shop(), request);
+        Shop shop = shopService.extractShopFromAuthentication(authentication);
+        themeService.removeTheme(shop, request);
         return ResponseEntity.ok(new BaseResponse(OK));
     }
 }
