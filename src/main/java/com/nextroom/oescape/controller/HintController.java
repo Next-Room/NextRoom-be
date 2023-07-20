12c84e03c99c103d265ce22c6f563a2caf89068b
Gibeom Lim <delphox60@gmail.com>
2023-07-20 14:40:50 +0900

    [FEATURE] createHint 메서드 구현

--- src/main/java/com/nextroom/oescape/controller/HintController.java
+++ src/main/java/com/nextroom/oescape/controller/HintController.java
@@ -1,8 +1,18 @@
 package com.nextroom.oescape.controller;
 
+import static com.nextroom.oescape.exceptions.StatusCode.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nextroom.oescape.domain.Shop;
+import com.nextroom.oescape.dto.BaseResponse;
+import com.nextroom.oescape.dto.HintDto;
 import com.nextroom.oescape.service.HintService;
 
 import lombok.RequiredArgsConstructor;
@@ -12,4 +22,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HintController {
     private final HintService hintService;
+
+    @PostMapping
+    public ResponseEntity<BaseResponse> addHint(
+        @AuthenticationPrincipal Shop shop,
+        @RequestBody HintDto.AddHintRequest request) {
+        hintService.addHint(shop, request);
+        return ResponseEntity.ok(new BaseResponse(OK));
+    }
 }
--- src/main/java/com/nextroom/oescape/service/HintService.java
+++ src/main/java/com/nextroom/oescape/service/HintService.java
@@ -20,15 +20,16 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class HintService {
-    private HintRepository hintRepository;
-    private ThemeRepository themeRepository;
+    private final HintRepository hintRepository;
+    private final ThemeRepository themeRepository;
 
     @Transactional
     public void addHint(Shop shop, HintDto.AddHintRequest request) {
-        validTheme(shop, request.getThemeId());
+        validateTheme(shop, request.getThemeId());
 
-        Theme theme = themeRepository.findById(request.getThemeId())
-            .orElseThrow(() -> new CustomException(THEME_NOT_FOUNT));
+        Theme theme = themeRepository.findById(
+                request.getThemeId()) // TODO optimize by making method get theme from shop
+            .orElseThrow(() -> new CustomException(THEME_NOT_FOUND));
 
         Hint hint = Hint.builder()
             .theme(theme)
@@ -42,9 +43,9 @@ public class HintService {
         hintRepository.save(hint);
     }
 
-    private void validTheme(Shop shop, Long themeId) {
-        if (themeRepository.existsByIdAndShop(themeId, shop)) {
-            throw new CustomException(THEME_NOT_FOUNT);
+    private void validateTheme(Shop shop, Long themeId) {
+        if (!themeRepository.existsByIdAndShop(themeId, shop)) {
+            throw new CustomException(THEME_NOT_FOUND);
         }
     }
 
