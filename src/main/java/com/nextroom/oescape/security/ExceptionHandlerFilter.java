73a4c111f6fc32dae6a9642deb7bf76343c9b761
Gibeom Lim <delphox60@gmail.com>
2023-08-07 12:59:44 +0900

    [HOTFIX] 토큰 검증 관련 에러 시 잘못된 응답 코드 수정

--- src/main/java/com/nextroom/oescape/security/ExceptionHandlerFilter.java
+++ src/main/java/com/nextroom/oescape/security/ExceptionHandlerFilter.java
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nextroom.oescape.exceptions.CustomException;
 import com.nextroom.oescape.exceptions.ErrorResponse;
 import com.nextroom.oescape.exceptions.StatusCode;
-import com.nextroom.oescape.exceptions.StatusCode;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
@@ -50,6 +49,8 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         objectMapper.getFactory().configure(JsonWriteFeature.ESCAPE_NON_ASCII.mappedFeature(), true);
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(statusCode.getCode().value());
+
         try {
             response.getWriter().write(objectMapper.writeValueAsString(
                     ErrorResponse.builder()
