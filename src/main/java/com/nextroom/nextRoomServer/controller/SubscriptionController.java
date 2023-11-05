8b5897297aef7f99a780812c12ee0f1f7636eb41
Gibeom Lim <delphox60@gmail.com>
2023-11-05 16:35:26 +0900

    [FEAT] google RTDN 수신용 컨트롤러 구현

--- src/main/java/com/nextroom/nextRoomServer/controller/SubscriptionController.java
+++ src/main/java/com/nextroom/nextRoomServer/controller/SubscriptionController.java
@@ -18,27 +18,10 @@ import com.nextroom.nextRoomServer.util.Base64Decoder;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-
 @Tag(name = "Subscription")
 @RestController
 @RequestMapping("/api/v1/subscription")
 @RequiredArgsConstructor
 public class SubscriptionController {
 
-    @PostMapping
-    public ResponseEntity<BaseResponse> updateSubscription(@RequestBody SubscriptionDto.UpdateSubscription messageDto) {
-        String decodedData = Base64Decoder.decode(messageDto.getMessage().getData());
-
-        System.out.println(decodedData);
-
-        Pattern pattern = Pattern.compile(".*\"testNotification\":\\{(.*?)\\}.*");
-        Matcher matcher = pattern.matcher(decodedData);
-
-        if (matcher.find()) {
-            String notificationContent = matcher.group(1);
-            System.out.println("Extracted Notification Content: " + notificationContent);
-        }
-
-        return ResponseEntity.ok(new BaseResponse(OK));
-    }
 }
