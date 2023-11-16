e5dd0c54357dbf15fb2fff4b2cab0a7a3dc4db3b
eunsol-an <dev.eunsol@gmail.com>
2023-11-17 00:49:25 +0900

    [FEAT] 구독 플랜 별 테마 제한 개수 추가(#90)

--- src/main/java/com/nextroom/nextRoomServer/enums/EnumModel.java
+++ src/main/java/com/nextroom/nextRoomServer/enums/EnumModel.java
@@ -12,4 +12,6 @@ public interface EnumModel {
     Integer getOriginPrice();
 
     Integer getSellPrice();
+
+    Integer getThemeLimitCount();
 }
--- src/main/java/com/nextroom/nextRoomServer/enums/SubscriptionPlan.java
+++ src/main/java/com/nextroom/nextRoomServer/enums/SubscriptionPlan.java
@@ -1,22 +1,25 @@
 package com.nextroom.nextRoomServer.enums;
 
 public enum SubscriptionPlan implements EnumModel {
-    MINI("mini_subscription", "미니", "2개의 테마를 등록할 수 있어요", 19900, 9900),
-    MEDIUM("medium_subscription", "미디움", "5개의 테마를 등록할 수 있어요", 29900, 14900),
-    LARGE("large_subscription", "라지", "8개의 테마를 등록할 수 있어요", 39900, 19900);
+    MINI("mini_subscription", "미니", "2개의 테마를 등록할 수 있어요", 19900, 9900, 2),
+    MEDIUM("medium_subscription", "미디움", "5개의 테마를 등록할 수 있어요", 29900, 14900, 3),
+    LARGE("large_subscription", "라지", "8개의 테마를 등록할 수 있어요", 39900, 19900, 4);
 
     private final String id;
     private final String plan;
     private final String description;
     private final Integer originPrice;
     private final Integer sellPrice;
+    private final Integer themeLimitCount;
 
-    SubscriptionPlan(String id, String plan, String description, Integer originPrice, Integer sellPrice) {
+    SubscriptionPlan(String id, String plan, String description, Integer originPrice, Integer sellPrice,
+        Integer themeLimitCount) {
         this.id = id;
         this.plan = plan;
         this.description = description;
         this.originPrice = originPrice;
         this.sellPrice = sellPrice;
+        this.themeLimitCount = themeLimitCount;
     }
 
     @Override
@@ -47,6 +50,11 @@ public enum SubscriptionPlan implements EnumModel {
     @Override
     public Integer getSellPrice() {
         return sellPrice;
+    }
+
+    @Override
+    public Integer getThemeLimitCount() {
+        return themeLimitCount;
     }
 
 }
