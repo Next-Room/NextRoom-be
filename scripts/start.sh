b926369cc9f7cb0c289480b43b8e4b31673b22dd
AN EUNSOL <dev.eunsol@gmail.com>
2024-04-27 21:25:23 +0900

    [CI/CD] 스크립트 실행 시 프로필 변경(prod)

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ echo "$TIME_NOW > $JAR_FILE file copied." >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 echo "$TIME_NOW > $JAR_FILE running application." >> $DEPLOY_LOG
-nohup java -jar -Dspring.profiles.active=dev $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > running process: $CURRENT_PID" >> $DEPLOY_LOG
