d632c2bfb9b2aeea24275347fb95d0a045563c5b
eunsol-an <dev.eunsol@gmail.com>
2024-07-19 03:26:35 +0900

    [CHORE] 구독 관련 로직 주석 제거

--- src/main/java/com/nextroom/nextRoomServer/controller/PaymentController.java
+++ src/main/java/com/nextroom/nextRoomServer/controller/PaymentController.java
@@ -1,71 +1,71 @@
-//package com.nextroom.nextRoomServer.controller;
-//
-//import static com.nextroom.nextRoomServer.exceptions.StatusCode.*;
-//
-//import java.io.IOException;
-//import java.util.Objects;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.nextroom.nextRoomServer.dto.BaseResponse;
-//import com.nextroom.nextRoomServer.dto.SubscriptionDto;
-//import com.nextroom.nextRoomServer.enums.SubscriptionStatus;
-//import com.nextroom.nextRoomServer.service.SubscriptionService;
-//import com.nextroom.nextRoomServer.util.Base64Decoder;
-//
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import lombok.RequiredArgsConstructor;
-//
-//@Tag(name = "Payment")
-//@RestController
-//@RequestMapping("/api/v1/payment")
-//@RequiredArgsConstructor
-//public class PaymentController {
-//
-//    private final SubscriptionService subscriptionService;
-//
-//    @PostMapping("/purchase")
-//    public ResponseEntity<BaseResponse> purchaseSubscription(
-//        @RequestBody SubscriptionDto.PurchaseSubscription request
-//    ) throws IOException {
-//        subscriptionService.purchaseSubscription(request.getPurchaseToken(), request.getSubscriptionId());
-//        return ResponseEntity.ok(new BaseResponse(OK));
-//    }
-//
-//    @PostMapping("/rtdn")
-//    public ResponseEntity<BaseResponse> updateSubscriptionPurchase(
-//        @RequestBody SubscriptionDto.UpdateSubscription requestBody
-//    ) throws
-//        Exception {
-//        String decodedData = Base64Decoder.decode(requestBody.getMessage().getData());
-//
-//        Pattern pattern = Pattern.compile(
-//            ".*\"subscriptionNotification\":\\{(.*?)\\}.*");
-//        Matcher matcher = pattern.matcher(decodedData);
-//
-//        String notificationContent = matcher.group(1);
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        SubscriptionDto.PublishedMessage publishedMessage = objectMapper.readValue(decodedData,
-//            SubscriptionDto.PublishedMessage.class);
-//        System.out.println(publishedMessage.getSubscriptionNotification().getPurchaseToken());
-//
-//        //     TODO handle exception
-//        Integer notificationType = publishedMessage.getSubscriptionNotification().getNotificationType();
-//        String purchaseToken = publishedMessage.getSubscriptionNotification().getPurchaseToken();
-//        String subscriptionId = publishedMessage.getSubscriptionNotification().getSubscriptionId();
-//
-//        if (Objects.equals(notificationType, SubscriptionStatus.SUBSCRIPTION_RENEWED.getStatus())) {
-//            subscriptionService.renew(purchaseToken, subscriptionId);
-//        } else if (Objects.equals(notificationType, SubscriptionStatus.SUBSCRIPTION_EXPIRED.getStatus())) {
-//            subscriptionService.expire(purchaseToken);
-//        }
-//        return ResponseEntity.ok(new BaseResponse(OK));
-//    }
-//}
+package com.nextroom.nextRoomServer.controller;
+
+import static com.nextroom.nextRoomServer.exceptions.StatusCode.*;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nextroom.nextRoomServer.dto.BaseResponse;
+import com.nextroom.nextRoomServer.dto.SubscriptionDto;
+import com.nextroom.nextRoomServer.enums.SubscriptionStatus;
+import com.nextroom.nextRoomServer.service.SubscriptionService;
+import com.nextroom.nextRoomServer.util.Base64Decoder;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Payment")
+@RestController
+@RequestMapping("/api/v1/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final SubscriptionService subscriptionService;
+
+    @PostMapping("/purchase")
+    public ResponseEntity<BaseResponse> purchaseSubscription(
+        @RequestBody SubscriptionDto.PurchaseSubscription request
+    ) throws IOException {
+        subscriptionService.purchaseSubscription(request.getPurchaseToken(), request.getSubscriptionId());
+        return ResponseEntity.ok(new BaseResponse(OK));
+    }
+
+    @PostMapping("/rtdn")
+    public ResponseEntity<BaseResponse> updateSubscriptionPurchase(
+        @RequestBody SubscriptionDto.UpdateSubscription requestBody
+    ) throws
+        Exception {
+        String decodedData = Base64Decoder.decode(requestBody.getMessage().getData());
+
+        Pattern pattern = Pattern.compile(
+            ".*\"subscriptionNotification\":\\{(.*?)\\}.*");
+        Matcher matcher = pattern.matcher(decodedData);
+
+        String notificationContent = matcher.group(1);
+        ObjectMapper objectMapper = new ObjectMapper();
+        SubscriptionDto.PublishedMessage publishedMessage = objectMapper.readValue(decodedData,
+            SubscriptionDto.PublishedMessage.class);
+        System.out.println(publishedMessage.getSubscriptionNotification().getPurchaseToken());
+
+        //     TODO handle exception
+        Integer notificationType = publishedMessage.getSubscriptionNotification().getNotificationType();
+        String purchaseToken = publishedMessage.getSubscriptionNotification().getPurchaseToken();
+        String subscriptionId = publishedMessage.getSubscriptionNotification().getSubscriptionId();
+
+        if (Objects.equals(notificationType, SubscriptionStatus.SUBSCRIPTION_RENEWED.getStatus())) {
+            subscriptionService.renew(purchaseToken, subscriptionId);
+        } else if (Objects.equals(notificationType, SubscriptionStatus.SUBSCRIPTION_EXPIRED.getStatus())) {
+            subscriptionService.expire(purchaseToken);
+        }
+        return ResponseEntity.ok(new BaseResponse(OK));
+    }
+}
--- src/main/java/com/nextroom/nextRoomServer/service/SubscriptionService.java
+++ src/main/java/com/nextroom/nextRoomServer/service/SubscriptionService.java
@@ -1,153 +1,154 @@
-//package com.nextroom.nextRoomServer.service;
-//
-//import static com.nextroom.nextRoomServer.enums.SubscriptionPlan.*;
-//import static com.nextroom.nextRoomServer.enums.UserStatus.*;
-//import static com.nextroom.nextRoomServer.exceptions.StatusCode.*;
-//
-//import java.io.IOException;
-//import java.time.LocalDate;
-//import java.time.ZonedDateTime;
-//import java.time.format.DateTimeFormatter;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import com.google.api.services.androidpublisher.model.SubscriptionPurchaseV2;
-//import com.nextroom.nextRoomServer.domain.Shop;
-//import com.nextroom.nextRoomServer.domain.Subscription;
-//import com.nextroom.nextRoomServer.dto.SubscriptionDto;
-//import com.nextroom.nextRoomServer.enums.EnumModel;
-//import com.nextroom.nextRoomServer.enums.SubscriptionPlan;
-//import com.nextroom.nextRoomServer.enums.UserStatus;
-//import com.nextroom.nextRoomServer.exceptions.CustomException;
-//import com.nextroom.nextRoomServer.repository.ShopRepository;
-//import com.nextroom.nextRoomServer.repository.SubscriptionRepository;
-//import com.nextroom.nextRoomServer.security.SecurityUtil;
-//import com.nextroom.nextRoomServer.util.AndroidPublisherClient;
-//import com.nextroom.nextRoomServer.util.Timestamped;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class SubscriptionService {
-//    private final SubscriptionRepository subscriptionRepository;
-//    private final ShopRepository shopRepository;
-////    private AndroidPublisherClient androidPublisherClient;
-//
-////    {
-////        try {
-////            androidPublisherClient = new AndroidPublisherClient();
-////        } catch (Exception e) {
-////            throw new RuntimeException(e);
-////        }
-////    }
-//
-//    public void test() {
-//        Shop shop = shopRepository.findByAdminCode("12321").orElseThrow(
-//            () -> new RuntimeException("ERROR"));
-//
-//        Subscription entity = Subscription.builder()
-//            .shop(shop)
-//            .status(SUBSCRIPTION)
-//            .plan(MINI)
-//            .expiryDate(LocalDate.now().plusDays(30))
-//            .build();
-//        subscriptionRepository.save(entity);
-//    }
-//
-//    @Transactional(readOnly = true)
-//    public SubscriptionDto.SubscriptionInfoResponse getSubscriptionInfo() {
-//        Long shopId = SecurityUtil.getRequestedShopId();
-//        Subscription subscription = subscriptionRepository.findByShopId(shopId).orElseThrow(
-//            () -> new CustomException(TARGET_SHOP_NOT_FOUND));
-//
-//        return new SubscriptionDto.SubscriptionInfoResponse(subscription);
-//    }
-//
-//    @Transactional
-//    public SubscriptionDto.UserStatusResponse getUserStatus() {
-//        Long shopId = SecurityUtil.getRequestedShopId();
-//        Subscription subscription = subscriptionRepository.findByShopId(shopId).orElseThrow(
-//            () -> new CustomException(TARGET_SHOP_NOT_FOUND));
-//
-//        checkUserStatus(subscription);
-//
-//        return new SubscriptionDto.UserStatusResponse(subscription);
-//    }
-//
-//    private void checkUserStatus(Subscription subscription) {
-//        UserStatus status = subscription.getStatus();
-//        LocalDate expiryDate = subscription.getExpiryDate();
-//
-//        if (status == FREE && expiryDate.isBefore(Timestamped.getToday())) {
-//            subscription.updateStatus(HOLD, expiryDate.plusYears(1), null);
-//            return;
-//        }
-//
-//        if (status == HOLD && expiryDate.isBefore(Timestamped.getToday())) {
-//            // subscriptionRepository.delete(subscription);
-//        }
-//    }
-//
-//    @Transactional(readOnly = true)
-//    public List<SubscriptionDto.SubscriptionPlanResponse> getSubscriptionPlan() {
-//        return toEnumValues(SubscriptionPlan.class);
-//    }
-//
-//    private List<SubscriptionDto.SubscriptionPlanResponse> toEnumValues(Class<? extends EnumModel> e) {
-//        return Arrays
-//            .stream(e.getEnumConstants())
-//            .map(SubscriptionDto.SubscriptionPlanResponse::new)
-//            .collect(Collectors.toList());
-//    }
-//
-//    public void purchaseSubscription(String purchaseToken, String subscriptionId) throws IOException {
-//        Long shopId = SecurityUtil.getRequestedShopId();
-//        Shop shop = shopRepository.findById(shopId).orElseThrow(() -> new CustomException(TARGET_HINT_NOT_FOUND));
-//
-//        SubscriptionPurchaseV2 purchase = androidPublisherClient.getSubscriptionPurchase(purchaseToken);
-//
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-//        ZonedDateTime zonedDateTime = ZonedDateTime.parse(purchase.getLineItems().get(0).getExpiryTime(), formatter);
-//        LocalDate expiryDate = zonedDateTime.toLocalDate();
-//
-//        String planId = purchase.getLineItems().get(0).getOfferDetails().getBasePlanId();
-//
-//        Subscription subscription = Subscription.builder()
-//            .shop(shop)
-//            .status(SUBSCRIPTION)
-//            .plan(SubscriptionPlan.getSubscriptionPlanByPlanId(planId))
-//            .expiryDate(expiryDate)
-//            .purchaseToken(purchaseToken)
-//            .build();
-//
-//        subscriptionRepository.save(subscription);
-//
-//        androidPublisherClient.acknowledgePurchase(purchaseToken, subscriptionId);
-//    }
-//
-//    public void renew(String purchaseToken, String subscriptionId) throws IOException {
-//        Subscription subscription = subscriptionRepository.findByPurchaseToken(purchaseToken)
-//            .orElseThrow(() -> new CustomException(TARGET_SHOP_NOT_FOUND));
-//
-//        SubscriptionPurchaseV2 purchase = androidPublisherClient.getSubscriptionPurchase(purchaseToken);
-//
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-//        ZonedDateTime zonedDateTime = ZonedDateTime.parse(purchase.getLineItems().get(0).getExpiryTime(), formatter);
-//        LocalDate expiryDate = zonedDateTime.toLocalDate();
-//
-//        subscription.renew(expiryDate);
-//
-//        androidPublisherClient.acknowledgePurchase(purchaseToken, subscriptionId);
-//    }
-//
-//    public void expire(String purchaseToken) {
-//        Subscription subscription = subscriptionRepository.findByPurchaseToken(purchaseToken)
-//            .orElseThrow(() -> new CustomException(TARGET_SHOP_NOT_FOUND));
-//        subscription.expire();
-//    }
-//}
+package com.nextroom.nextRoomServer.service;
+
+import static com.nextroom.nextRoomServer.enums.SubscriptionPlan.*;
+import static com.nextroom.nextRoomServer.enums.UserStatus.*;
+import static com.nextroom.nextRoomServer.exceptions.StatusCode.*;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.google.api.services.androidpublisher.model.SubscriptionPurchaseV2;
+import com.nextroom.nextRoomServer.domain.Shop;
+import com.nextroom.nextRoomServer.domain.Subscription;
+import com.nextroom.nextRoomServer.dto.SubscriptionDto;
+import com.nextroom.nextRoomServer.enums.EnumModel;
+import com.nextroom.nextRoomServer.enums.SubscriptionPlan;
+import com.nextroom.nextRoomServer.enums.UserStatus;
+import com.nextroom.nextRoomServer.exceptions.CustomException;
+import com.nextroom.nextRoomServer.repository.ShopRepository;
+import com.nextroom.nextRoomServer.repository.SubscriptionRepository;
+import com.nextroom.nextRoomServer.security.SecurityUtil;
+import com.nextroom.nextRoomServer.util.Timestamped;
+import com.nextroom.nextRoomServer.util.inapp.AndroidPurchaseUtils;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SubscriptionService {
+    private final SubscriptionRepository subscriptionRepository;
+    private final ShopRepository shopRepository;
+    private final AndroidPurchaseUtils androidPurchaseUtils;
+    //    private AndroidPublisherClient androidPublisherClient;
+
+    //    {
+    //        try {
+    //            androidPublisherClient = new AndroidPublisherClient();
+    //        } catch (Exception e) {
+    //            throw new RuntimeException(e);
+    //        }
+    //    }
+
+    public void test() {
+        Shop shop = shopRepository.findByAdminCode("12321").orElseThrow(
+            () -> new RuntimeException("ERROR"));
+
+        Subscription entity = Subscription.builder()
+            .shop(shop)
+            .status(SUBSCRIPTION)
+            .plan(MINI)
+            .expiryDate(LocalDate.now().plusDays(30))
+            .build();
+        subscriptionRepository.save(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public SubscriptionDto.SubscriptionInfoResponse getSubscriptionInfo() {
+        Long shopId = SecurityUtil.getCurrentShopId();
+        Subscription subscription = subscriptionRepository.findByShopId(shopId).orElseThrow(
+            () -> new CustomException(TARGET_SHOP_NOT_FOUND));
+
+        return new SubscriptionDto.SubscriptionInfoResponse(subscription);
+    }
+
+    @Transactional
+    public SubscriptionDto.UserStatusResponse getUserStatus() {
+        Long shopId = SecurityUtil.getCurrentShopId();
+        Subscription subscription = subscriptionRepository.findByShopId(shopId).orElseThrow(
+            () -> new CustomException(TARGET_SHOP_NOT_FOUND));
+
+        checkUserStatus(subscription);
+
+        return new SubscriptionDto.UserStatusResponse(subscription);
+    }
+
+    private void checkUserStatus(Subscription subscription) {
+        UserStatus status = subscription.getStatus();
+        LocalDate expiryDate = subscription.getExpiryDate();
+
+        if (status == FREE && expiryDate.isBefore(Timestamped.getToday())) {
+            subscription.updateStatus(HOLD, expiryDate.plusYears(1), null);
+            return;
+        }
+
+        if (status == HOLD && expiryDate.isBefore(Timestamped.getToday())) {
+            // subscriptionRepository.delete(subscription);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubscriptionDto.SubscriptionPlanResponse> getSubscriptionPlan() {
+        return toEnumValues(SubscriptionPlan.class);
+    }
+
+    private List<SubscriptionDto.SubscriptionPlanResponse> toEnumValues(Class<? extends EnumModel> e) {
+        return Arrays
+            .stream(e.getEnumConstants())
+            .map(SubscriptionDto.SubscriptionPlanResponse::new)
+            .collect(Collectors.toList());
+    }
+
+    public void purchaseSubscription(String purchaseToken, String subscriptionId) throws IOException {
+        Long shopId = SecurityUtil.getCurrentShopId();
+        Shop shop = shopRepository.findById(shopId).orElseThrow(() -> new CustomException(TARGET_HINT_NOT_FOUND));
+
+        SubscriptionPurchaseV2 purchase = androidPurchaseUtils.verify(purchaseToken);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(purchase.getLineItems().get(0).getExpiryTime(), formatter);
+        LocalDate expiryDate = zonedDateTime.toLocalDate();
+
+        String planId = purchase.getLineItems().get(0).getOfferDetails().getBasePlanId();
+
+        Subscription subscription = Subscription.builder()
+            .shop(shop)
+            .status(SUBSCRIPTION)
+            .plan(SubscriptionPlan.getSubscriptionPlanByPlanId(planId))
+            .expiryDate(expiryDate)
+            .purchaseToken(purchaseToken)
+            .build();
+
+        subscriptionRepository.save(subscription);
+
+        androidPurchaseUtils.acknowledge(purchaseToken, subscriptionId);
+    }
+
+    public void renew(String purchaseToken, String subscriptionId) throws IOException {
+        Subscription subscription = subscriptionRepository.findByPurchaseToken(purchaseToken)
+            .orElseThrow(() -> new CustomException(TARGET_SHOP_NOT_FOUND));
+
+        SubscriptionPurchaseV2 purchase = androidPurchaseUtils.verify(purchaseToken);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(purchase.getLineItems().get(0).getExpiryTime(), formatter);
+        LocalDate expiryDate = zonedDateTime.toLocalDate();
+
+        subscription.renew(expiryDate);
+
+        androidPurchaseUtils.acknowledge(purchaseToken, subscriptionId);
+    }
+
+    public void expire(String purchaseToken) {
+        Subscription subscription = subscriptionRepository.findByPurchaseToken(purchaseToken)
+            .orElseThrow(() -> new CustomException(TARGET_SHOP_NOT_FOUND));
+        subscription.expire();
+    }
+}
