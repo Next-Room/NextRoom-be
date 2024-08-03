58df8c645a0b28a9a7263b2ac4d283b79f0ad231
rachel3486 <rachel3486@gmail.com>
2024-08-03 21:29:23 +0900

    [FEAT] Add not null constraint

--- src/main/java/com/nextroom/nextRoomServer/domain/Subscription.java
+++ src/main/java/com/nextroom/nextRoomServer/domain/Subscription.java
@@ -40,8 +40,11 @@ public class Subscription extends Timestamped {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private SubscriptionPlan plan;
+    @Column(nullable = false)
     private LocalDate startDate;
+    @Column(nullable = false)
     private LocalDate expiryDate;
 
     public void renew(LocalDate expiryDate) {
