f42f5cd538d61b6bd941fe1893127c41b4cf18a5
eunsol-an <dev.eunsol@gmail.com>
2024-08-10 21:52:27 +0900

    [DOCS] Swagger response 구체적 명시

--- src/main/java/com/nextroom/nextRoomServer/controller/SubscriptionController.java
+++ src/main/java/com/nextroom/nextRoomServer/controller/SubscriptionController.java
@@ -2,6 +2,9 @@ package com.nextroom.nextRoomServer.controller;
 
 import static com.nextroom.nextRoomServer.exceptions.StatusCode.*;
 
+import com.nextroom.nextRoomServer.dto.SubscriptionDto;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +29,7 @@ public class SubscriptionController {
     @Operation(
         summary = "구독 정보 조회(마이 페이지)",
         responses = {
-            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SubscriptionDto.SubscriptionInfoResponse.class))),
             @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
             @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
             @ApiResponse(responseCode = "404", description = "TARGET_SHOP_NOT_FOUND")
@@ -40,7 +43,7 @@ public class SubscriptionController {
     @Operation(
         summary = "유저 상태 조회",
         responses = {
-            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SubscriptionDto.UserStatusResponse.class))),
             @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
             @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
             @ApiResponse(responseCode = "404", description = "TARGET_SHOP_NOT_FOUND")
@@ -54,7 +57,7 @@ public class SubscriptionController {
     @Operation(
         summary = "구독 요금제 조회",
         responses = {
-            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SubscriptionDto.SubscriptionPlanResponse.class))),
             @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
             @ApiResponse(responseCode = "403", description = "NOT_PERMITTED")
         }
