353634f6ac15a5bd4646d02c10dcd5f856d8c18e
eunsol-an <dev.eunsol@gmail.com>
2024-08-13 23:40:39 +0900

    [DOCS] response type 명시

--- src/main/java/com/nextroom/nextRoomServer/controller/SubscriptionController.java
+++ src/main/java/com/nextroom/nextRoomServer/controller/SubscriptionController.java
@@ -19,6 +19,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Tag(name = "Subscription")
 @RestController
 @RequestMapping("/api/v1/subscription")
@@ -29,41 +31,41 @@ public class SubscriptionController {
     @Operation(
         summary = "구독 정보 조회(마이 페이지)",
         responses = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SubscriptionDto.SubscriptionInfoResponse.class))),
-            @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
-            @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
-            @ApiResponse(responseCode = "404", description = "TARGET_SHOP_NOT_FOUND")
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED", content = @Content),
+            @ApiResponse(responseCode = "403", description = "NOT_PERMITTED", content = @Content),
+            @ApiResponse(responseCode = "404", description = "TARGET_SHOP_NOT_FOUND", content = @Content)
         }
     )
     @GetMapping("/mypage")
-    public ResponseEntity<BaseResponse> getSubscriptionInfo() {
+    public ResponseEntity<DataResponse<SubscriptionDto.SubscriptionInfoResponse>> getSubscriptionInfo() {
         return ResponseEntity.ok(new DataResponse<>(OK, subscriptionService.getSubscriptionInfo()));
     }
 
     @Operation(
         summary = "유저 상태 조회",
         responses = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SubscriptionDto.UserStatusResponse.class))),
-            @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
-            @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
-            @ApiResponse(responseCode = "404", description = "TARGET_SHOP_NOT_FOUND")
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED", content = @Content),
+            @ApiResponse(responseCode = "403", description = "NOT_PERMITTED", content = @Content),
+            @ApiResponse(responseCode = "404", description = "TARGET_SHOP_NOT_FOUND", content = @Content)
         }
     )
     @GetMapping("/status")
-    public ResponseEntity<BaseResponse> getUserStatus() {
+    public ResponseEntity<DataResponse<SubscriptionDto.UserStatusResponse>> getUserStatus() {
         return ResponseEntity.ok(new DataResponse<>(OK, subscriptionService.getUserStatus()));
     }
 
     @Operation(
         summary = "구독 요금제 조회",
         responses = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SubscriptionDto.SubscriptionPlanResponse.class))),
-            @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
-            @ApiResponse(responseCode = "403", description = "NOT_PERMITTED")
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED", content = @Content),
+            @ApiResponse(responseCode = "403", description = "NOT_PERMITTED", content = @Content)
         }
     )
     @GetMapping("/plan")
-    public ResponseEntity<BaseResponse> getSubscriptionPlan() {
+    public ResponseEntity<DataResponse<List<SubscriptionDto.SubscriptionPlanResponse>>> getSubscriptionPlan() {
         return ResponseEntity.ok(new DataResponse<>(OK, subscriptionService.getSubscriptionPlan()));
     }
 }
