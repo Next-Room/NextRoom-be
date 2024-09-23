e73ab97a1a056921b7bc9ca9c7bf5a9f82e122dc
yoojjang <yoojjang@smilegate.com>
2024-09-24 01:07:26 +0900

    [FEAT] Add banner api

---
+++ src/main/java/com/nextroom/nextRoomServer/domain/Banner.java
@@ -0,0 +1,35 @@
+package com.nextroom.nextRoomServer.domain;
+
+import com.nextroom.nextRoomServer.util.Timestamped;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Banner extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "banner_id", nullable = false)
+    private Long id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+}
---
+++ src/main/java/com/nextroom/nextRoomServer/repository/BannerRepository.java
@@ -0,0 +1,11 @@
+package com.nextroom.nextRoomServer.repository;
+
+import com.nextroom.nextRoomServer.domain.Banner;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<Banner, Long> {
+
+    List<Banner> findAllByActiveTrue();
+
+}
