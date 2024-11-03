c420301c859b1ac93aed08fd721916a55955d536
rachel3486 <rachel3486@gmail.com>
2024-11-03 23:16:16 +0900

    [REFACTOR] Add NPE

--- src/main/java/com/nextroom/nextRoomServer/util/Base64Decoder.java
+++ src/main/java/com/nextroom/nextRoomServer/util/Base64Decoder.java
@@ -5,6 +5,9 @@ import java.util.Base64;
 
 public class Base64Decoder {
     public static String decode(String base64Data) {
+        if (base64Data == null) {
+            return null;
+        }
         // Base64 디코딩
         byte[] decodedBytes = Base64.getDecoder().decode(base64Data);
         String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
