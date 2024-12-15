30c7b4ee18d9dc1fe740c14c7377b93065cb01ab
eunsol-an <dev.eunsol@gmail.com>
2024-12-15 16:00:52 +0900

    [DOCS] 테마 타이머 배경 Swagger 설명 추가

--- src/main/java/com/nextroom/nextRoomServer/controller/ThemeController.java
+++ src/main/java/com/nextroom/nextRoomServer/controller/ThemeController.java
@@ -94,23 +94,64 @@ public class ThemeController {
         return ResponseEntity.ok(new BaseResponse(OK));
     }
 
+    @Operation(
+            summary = "테마 타이머 배경 이미지 PreSigned Url 요청",
+            description = """
+                    s3 url /{profile}/{shopId}/{themeId}/{type}/{num}_uuid.png
+
+                    ex) "/dev/1/3/timer/1_2e20b6a9-e24b-45a8-a974-005c14f9f44f.png"
+                    """,
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
+                    @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
+                    @ApiResponse(responseCode = "404", description = "TARGET_THEME_NOT_FOUND")
+            }
+    )
     @GetMapping("/timer/url/{themeId}")
     public ResponseEntity<DataResponse<ThemeUrlResponse>> getUrl(@PathVariable Long themeId) {
         return ResponseEntity.ok(new DataResponse<>(OK, themeService.getTimerUrl(themeId)));
     }
 
+    @Operation(
+            summary = "테마 타이머 배경 이미지 추가",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
+                    @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
+                    @ApiResponse(responseCode = "404", description = "TARGET_THEME_NOT_FOUND")
+            }
+    )
     @PostMapping("/timer")
     public ResponseEntity<BaseResponse> addThemeTimerImage(@RequestBody @Valid ThemeDto.ThemeUrlRequest request) {
         themeService.addThemeTimerImage(request);
         return ResponseEntity.ok(new BaseResponse(OK));
     }
 
+    @Operation(
+            summary = "테마 타이머 배경 이미지 삭제",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
+                    @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
+                    @ApiResponse(responseCode = "404", description = "TARGET_THEME_NOT_FOUND")
+            }
+    )
     @DeleteMapping("/timer/{themeId}")
     public ResponseEntity<BaseResponse> removeTheme(@PathVariable Long themeId) {
         themeService.removeThemeTimerImage(themeId);
         return ResponseEntity.ok(new BaseResponse(OK));
     }
 
+    @Operation(
+            summary = "(앱) 테마 타이머 배경 on/off 설정",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "401", description = "TOKEN_UNAUTHORIZED"),
+                    @ApiResponse(responseCode = "403", description = "NOT_PERMITTED"),
+                    @ApiResponse(responseCode = "404", description = "TARGET_THEME_NOT_FOUND")
+            }
+    )
     @PutMapping("/timer/active")
     public ResponseEntity<BaseResponse> activeTimerUrl(@RequestBody @Valid ThemeDto.ThemeActiveUrlRequest request) {
         themeService.activeThemeTimerUrl(request);
